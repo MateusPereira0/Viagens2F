de8526cf63eb3d14787342a55488b8a96ad5c175
Mateus Pereira <126568688+MateusPereira0@users.noreply.github.com>
2023-10-29 13:21:33 -0300

    atualizacao

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,39 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-       // Classe Objeto = Construtor
-        Turista mochileiro = new Turista("Lindsay");
-        Turista mochileira = new Turista();
 
-        // Definir forma de cada turista
+        //Classe Objeto = Construtor
+        Turista mochileiro = new Turista("Lindsay");
+         Turista mochileira = new Turista();
+
+      //Definir cada turista
         mochileiro.setCpf(Validacao.cpf("123"));
          mochileira.setNome("Namor");
-         mochileira.setCpf("456");
+           mochileira.setCpf("456") ;
 
-        // Definir comportamento
-         String retorno = mochileira.viajar();
-        JOptionPane.showMessageDialog(null,retorno);
+      //Definir comportamento
+        String retorno = mochileira.viajar();
+         JOptionPane.showMessageDialog(null,retorno);
+
+    //Atributo dinâmica
+        String nome = JOptionPane.showInputDialog("Entre com o nome da pessoa Turista:");
+
+         Turista pessoaTurista = new Turista(nome);
+
+       //Saída de dados - nome armazenado acima
+         System.out.println(pessoaTurista.getNome());
+
+       //Pedir cpf e mostrar
+         String cpf = JOptionPane.showInputDialog("Entre com o cpf:");
+
+       //Adicionar cpf no objeto pessoaTurista
+         if (Validacao.cpf(cpf)){
+             pessoaTurista.setCpf(cpf);
+              System.out.println(pessoaTurista.getCpf());
+               System.out.println("CPF Válido");
+         }
+          else {
+            System.out.println("CPF inválido!");
+        }
     }
-}
+ }
